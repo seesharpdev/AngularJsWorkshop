9ccad798058e18555e0fb7d57d8995e7689e0e13
csouto <carlos.p.souto@gmail.com>
2015-03-09 15:27:13 +0000

    Initial Commit

---
+++ AngularJsWorkshop/AngularJs.Host/Js/script.js
@@ -0,0 +1,17 @@
+﻿(function() {
+
+    var app = angular.module("Home");
+
+    var HomeController = function() {
+
+    };
+
+    var index = function() {
+
+    };
+
+    return {
+        Index: index
+    }
+
+}(Quest.WebApp = Quest.WebApp || {}))
